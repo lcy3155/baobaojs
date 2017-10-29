00c1e2ee70b1c8fd19b3b5ee7e90312894127327
lcy3155 <315508360@qq.com>
2017-10-29 20:14:01 +0800

    baobao.js

---
+++ routes/item.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var router = express.Router();
+var mysql      = require('mysql');
+var connection = mysql.createPool({
+  host     : 'localhost',
+  user     : 'root',
+  password : '123456' ,
+  database:'item'
+});
+
+/* GET home page. */
+router.get('/list',function(req, res, next) {
+    res.header('Access-Control-Allow-Origin',"*")
+    connection.query('SELECT id,name FROM baobao',function(err, rows, fields) {
+        if(err) throw err
+        res.send(rows)
+      })
+});
+
+router.post('/detail', function(req, res, next) {
+    res.header('Access-Control-Allow-Origin',"*")
+    //console.log(req.body.id)
+
+    connection.query('SELECT detail FROM baobao where id='+req.body.id,
+        function(err, rows, fields) {
+            if(err) throw err
+            res.send(rows)
+        })
+});
+
+module.exports = router;
